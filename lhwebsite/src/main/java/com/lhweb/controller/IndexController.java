ce139519c70ebfabd952ab89ea3f55ea5d916d8e
halfThread <liuhuan0672@126.com>
2015-08-13 09:52:21 +0800

    Build project structure
    建立工程架构

---
+++ lhwebsite/src/main/java/com/lhweb/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.lhweb.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by liuhuan on 2015/8/12.
+ */
+@Controller("/index")
+public class IndexController {
+    private Logger logger = LoggerFactory.getLogger(IndexController.class);
+
+    @RequestMapping("/")
+    public String index(){
+        logger.info("Hello world. This is my first website!");
+        return "index";
+    }
+}
